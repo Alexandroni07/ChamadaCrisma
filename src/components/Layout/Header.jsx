2067894d893646fc4b0fd75f5c35d00c3f71fafb
Alexandroni07 <gaalexandroni@gmail.com>
2025-05-09 15:25:56 -0300

    Rename Routes, add Header(no test) and Navbar(no test)

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header className="site-header">
+      <div className="header-content">
+        <h1>Crisma Paróquia Nossa Senhora da Assunção - Aguas Claras </h1>
+      </div>
+    </header>
+  )
+}
